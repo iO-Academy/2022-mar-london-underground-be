83ed0824110cb8778fc0e8c0dbdab0abbee8bda4
Gediminas Melinauskas <g.melinauskas0895@gmail.com>
2022-06-21 09:29:46 +0100

    fixed the journeys total time

--- services/tubeService.js
+++ services/tubeService.js
@@ -16,28 +16,39 @@ const getJourneys = async (start, end) => {
     return await tubeRepository.getJourneys(start, end)
         .then((journeys) => {
             let lines = [];
+            let stop;
             journeys.forEach(line => {
                 let filteredStations = [];
                 let journeyTime = 0;
                 if (start > end) {
                     filteredStations = line.stations.filter(filtered => filtered.name <= start && filtered.name >= end);
+                    filteredStations[filteredStations.length - 1].timeToPrev = 0;  //6478/4853 before reduce
                     journeyTime = filteredStations.reduce((sum, current) => sum + current.timeToPrev, 0);
+                    stops = filteredStations.reduce((stations, station) => {
+                        let stopData = {"stop": station.name, "timeToNext": station.timeToPrev}
+                        stations.push(stopData);
+                        return stations;
+                    }, []);
+
                 } else {
                     filteredStations = line.stations.filter(filtered => filtered.name >= start && filtered.name <= end);
+                    filteredStations[filteredStations.length - 1].timeToNext = 0; //6673/4997 before reduce
                     journeyTime = filteredStations.reduce((sum, current) => sum + current.timeToNext, 0);
-                }
-               let stops = filteredStations.reduce((stations, station) => {
+                    stops = filteredStations.reduce((stations, station) => {
                         let stopData = {"stop": station.name, "timeToNext": station.timeToNext}
                         stations.push(stopData);
-                    return stations;
-                }, [])
+                        return stations;
+                    }, []);
+                }
+
                 console.log(stops);
                 console.log(journeyTime);
                 let numStops = filteredStations.length -1;
-                console.log('number of stops = ' + numStops);
-                let lineData = {"line": line.line, "stops": numStops, "time": journeyTime}
-                lines.push(lineData);
 
+                // console.log(stops);
+                console.log('number of stops = ' + numStops);
+                let lineData = {"line": line.line, "stops": numStops, "time": journeyTime,}
+                lines.push(lineData);
             })
             return lines;
         })
